f7a03ce9c187c8c9e85d8084141d7ce4515c2f97
a1 <linhuachon@163.com>
2017-11-08 11:30:39 +0800

    添加一个学生类

---
+++ app/src/main/java/com/harlan/lhc/studegreedao/bean/Student.java
@@ -0,0 +1,59 @@
+package com.harlan.lhc.studegreedao.bean;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * Created by a1 on 2017/11/8.
+ */
+@Entity
+public class Student {
+    @Id
+    private Long id;
+    private String name;
+    private String sex;
+    private String address;
+    private String age;
+    public String getAge() {
+        return this.age;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getSex() {
+        return this.sex;
+    }
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    @Generated(hash = 1788955119)
+    public Student(Long id, String name, String sex, String address, String age) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.address = address;
+        this.age = age;
+    }
+    @Generated(hash = 1556870573)
+    public Student() {
+    }
+}
